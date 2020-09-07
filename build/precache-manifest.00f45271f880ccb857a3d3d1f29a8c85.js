dbab505bd67d42a68688c069bbf338a5d7cb2021
abdul kader anas <68795535+sakaMicro@users.noreply.github.com>
2020-09-08 01:22:11 +0300

    good

---
+++ build/precache-manifest.00f45271f880ccb857a3d3d1f29a8c85.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "beed99dd9af4c2b9bd89da1a7a13f188",
+    "url": "/CleverProgrammers/react-portfolioindex.html"
+  },
+  {
+    "revision": "0157c458706b76babd3a",
+    "url": "/CleverProgrammers/react-portfoliostatic/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "f4f234c47dfca8fe5360",
+    "url": "/CleverProgrammers/react-portfoliostatic/js/2.3ef2d018.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/CleverProgrammers/react-portfoliostatic/js/2.3ef2d018.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0157c458706b76babd3a",
+    "url": "/CleverProgrammers/react-portfoliostatic/js/main.181f9586.chunk.js"
+  },
+  {
+    "revision": "f703a6c0542d67c23549",
+    "url": "/CleverProgrammers/react-portfoliostatic/js/runtime-main.d3f33434.js"
+  }
+]);
